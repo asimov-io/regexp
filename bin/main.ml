8aef757732c47092b44fa72c7a06b47a6d77f61d
rubikbeggar <sebonduelle@free.fr>
2024-01-08 18:57:21 +0100

    Nearly working with unit/fail

--- bin/main.ml
+++ bin/main.ml
@@ -1,32 +1,44 @@
 open Regexp
 
-module M = Necromonads.ID
+module M = Necromonads.List
 
 module Types = struct
   type symb = char
   type lett = char
-  type word = Buffer.t
+  type word = lett list
 end
 
 module Spec = struct
   include Unspec(M)(Types)
 
-  let lett_of_symb (s: symb) = s
+  let lett_of_symb (s: symb) = M.ret s
 
   let equal ((s: symb), (l: lett)) =
-    match (s = l) with
-    | true -> True
-    | false -> False
+    if (s = l) then M.ret () else M.fail ()
+
+  let head (w: word) = match w with
+    | [] -> M.fail ()
+    | l :: _ -> M.ret l
   
-  let isempty (w: word) =
-    if w.length = 0 then M.ret () else M.fail ()
-  
-  let head (w: word) =
+  let tail (w: word) = match w with
+    | [] -> M.fail ()
+    | _ :: t -> M.ret t
+
+  let isempty (w: word) = match w with
+    | [] -> M.ret ()
+    | _ -> M.fail ()
     
 end
 
 open MakeInterpreter(Spec)
 
-let t1 = Plus(Plus(Symb 'a', Symb 'b'), Symb 'c'), 
 
-let test 
+let test t =
+  let _ = M.extract (eval t) in
+  Printf.printf "Reconnu\n"
+
+
+let t1 = Plus(Plus(Symb 'a', Symb 'b'), Symb 'c'), ['d']
+
+
+let _ = test t1
